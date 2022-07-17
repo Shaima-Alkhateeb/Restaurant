a749a747a5908fe8873e0ca40fc4246945a4a9da
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-07-18 00:06:37 +0300

    add localStorage branch

--- app.js
+++ app.js
@@ -53,10 +53,30 @@ function handleSubmit(event) {
 
     console.log(newFood);
     newFood.render();
+    saveData();
+}
+
+function saveData() {
+    let strifyedData = JSON.stringify(allFoods);
+    localStorage.setItem("Foods", strifyedData);
+}
+
+function getData() {
+    
+    let retrivedData = localStorage.getItem("Foods");
+    let parsedData = JSON.parse(retrivedData)
+    console.log(parsedData);
+    console.log(typeof parsedData); 
+    
+    if (parsedData != null) {
+        for (let i = 0; i < parsedData.length; i++) {
+
+            new Food(parsedData[i].foodName, parsedData[i].type, parsedData[i].price, parsedData[i].foodId);
+        }
+    }
 }
 
 
 for (let i = 0; i < allFoods.length; i++){
     allFoods[i].render();
 }
-
