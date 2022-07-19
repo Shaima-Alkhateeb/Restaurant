ae361c4da06c80c5f54b1c8532910c0421554d0c
Shaima Alkhateeb <shaimaalkhateeb535@gmail.com>
2022-07-19 14:33:45 +0300

    add some line in chartJS

--- statistics.js
+++ statistics.js
@@ -1,180 +1,129 @@
 'use strict';
 
-const allFoods = [];
-const parentEl = document.getElementById("menu");
 
-// let randomFoodID = 10000;
-function Food( foodName, type, price){
-    this.foodName = foodName;
-    this.type = type;
-    this.price = price;
-    this.foodId =  this.randomFoodID();   // randomFoodID++;
-    
-    allFoods.push(this);
-}
+// const parentEl = document.getElementById("menu");
+let retrivedData = localStorage.getItem("foodKey");
+let parsedData = JSON.parse(retrivedData);
 
-Food.prototype.randomFoodID = function () {
-    return(Math.floor(1000 + Math.random() * 9000));
-}
-
-
-Food.prototype.render = function () {
-    
-    let tablePlace = document.getElementById("tBodyID");
-
-    let tableForRow = document.createElement("tr");
-    let tablecontent1 = document.createElement("td");
-    let tablecontent2 = document.createElement("td");
-    let tablecontent3 = document.createElement("td");
-    let tablecontent4 = document.createElement("td");
-
-    tablecontent1.textContent = this.foodId;
-    tablecontent2.textContent = this.foodName;
-    tablecontent3.textContent = this.type;
-    tablecontent4.textContent = this.price;
-
-    tableForRow.appendChild(tablecontent1);
-    tableForRow.appendChild(tablecontent2);
-    tableForRow.appendChild(tablecontent3);
-    tableForRow.appendChild(tablecontent4);
-    tablePlace.appendChild(tableForRow);
-}
-
-// Event Listener
-let formFood = document.getElementById("formID");
-formFood.addEventListener('submit',handleSubmit);
-
-function handleSubmit(event) {
-    event.preventDefault();
-    // let food = document.getElementById("formID");
-    
-    let foodName = event.target.foodname.value;
-    let type = event.target.food.value;
-    let price = event.target.price.value;
-
-    const newFood = new Food( foodName, type, price);
-
-    console.log(newFood);
-    newFood.render();
-    saveData();
-}
-
-// save my data --> local storage
-function saveData() {
-    let strifyedData = JSON.stringify(allFoods);
-    localStorage.setItem("foodKey", strifyedData);
-}
-
-
-// 
-function getData() {
-    
-    let retrivedData = localStorage.getItem("foodKey");
-    let parsedData = JSON.parse(retrivedData)
-    // console.log(parsedData);
-    // console.log(typeof parsedData); 
-    
+function getData() {  
     // re-istantioation
     if (parsedData != null) {
         for (let i = 0; i < parsedData.length; i++) {
+            const allFoods = [];
+            let tablePlace = document.getElementById("tBodyID");
 
-            new Food(parsedData[i].foodName, parsedData[i].type, parsedData[i].price, parsedData[i].foodId);
+            let tableForRow = document.createElement("tr");
+            let tablecontent1 = document.createElement("td");
+            let tablecontent2 = document.createElement("td");
+            let tablecontent3 = document.createElement("td");
+            let tablecontent4 = document.createElement("td");
+        
+            tablecontent1.textContent = allFoods[i].foodId;
+            tablecontent2.textContent = allFoods[i].foodName;
+            tablecontent3.textContent = allFoods[i].type;
+            tablecontent4.textContent = allFoods[i].price;
+        
+            tableForRow.appendChild(tablecontent1);
+            tableForRow.appendChild(tablecontent2);
+            tableForRow.appendChild(tablecontent3);
+            tableForRow.appendChild(tablecontent4);
+            tablePlace.appendChild(tableForRow);
+
+            // new Food(parsedData[i].foodName, parsedData[i].type, parsedData[i].price, parsedData[i].foodId);
         }
-    }
-    for (let i = 0; i < allFoods.length; i++){
-        allFoods[i].render();
-        console.log(allFoods);
     }
 }
 
 getData();
 
+//////////////////////////////////////////////////////
+
 
 // Line chart JS
-const chartNames = [];
-const chartPrices = [];
-const chartTypes = [];
-for (let i=0; i<allFoods[i]; i++) {
-    chartNames.push(allFoods[i].foodName);
-    chartPrices.push(allFoods[i].price);
-    chartTypes.push(allFoods[i].type)
+// const chartNames = [];
+// const chartPrices = [];
+// const chartTypes = [];
+// for (let i=0; i<allFoods[i]; i++) {
+//     chartNames.push(allFoods[i].foodName);
+//     chartPrices.push(allFoods[i].price);
+//     chartTypes.push(allFoods[i].type)
 
-}
+// }
 
-  const data = {
-    labels: chartNames,
-    datasets: [{
-      label: 'Price',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: chartPrices,
-    }]
-  };
+//   const data = {
+//     labels: chartNames,
+//     datasets: [{
+//       label: 'Price',
+//       backgroundColor: 'rgb(255, 99, 132)',
+//       borderColor: 'rgb(255, 99, 132)',
+//       data: chartPrices,
+//     }]
+//   };
 
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
+//   const config = {
+//     type: 'line',
+//     data: data,
+//     options: {}
+//   };
 
-  const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+//   const myChart = new Chart(
+//     document.getElementById('myChart'),
+//     config
+//   );
 
-  // Bar chart JS
-  const numberOfEachTypeOfFood = [];
+//   // Bar chart JS
+//   const numberOfEachTypeOfFood = [];
 
-  fruitAndVegetablesCounting = 0;
-  starchyFoodCounting = 0;
-  dairyCounting = 0;
-  proteinCounting = 0;
-  fatCounting = 0;
+//   fruitAndVegetablesCounting = 0;
+//   starchyFoodCounting = 0;
+//   dairyCounting = 0;
+//   proteinCounting = 0;
+//   fatCounting = 0;
 
-switch(allFoods[i].type) {
-    case "Fruit and vegetables":
-        fruitAndVegetablesCounting++;
-      break;
+// switch(allFoods[i].type) {
+//     case "Fruit and vegetables":
+//         fruitAndVegetablesCounting++;
+//       break;
 
-    case "Starchy food":
-        starchyFoodCounting++;
-      break;
+//     case "Starchy food":
+//         starchyFoodCounting++;
+//       break;
 
-    case "Dairy":
-        dairyCounting++;
-      break;
+//     case "Dairy":
+//         dairyCounting++;
+//       break;
 
-    case "Protein":
-        proteinCounting++;
-      break;  
+//     case "Protein":
+//         proteinCounting++;
+//       break;  
 
-    case "Fat":
-        fatCounting++;
-      break;
+//     case "Fat":
+//         fatCounting++;
+//       break;
      
-    default:
-      // code block
-  }
+//     default:
+//       // code block
+//   }
 
   
 
-  const dataBar = {
-    labels: chartTypes,
-    datasets: [{
-      label: 'Type',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: chartPrices,
-    }]
-  };
+//   const dataBar = {
+//     labels: chartTypes,
+//     datasets: [{
+//       label: 'Type',
+//       backgroundColor: 'rgb(255, 99, 132)',
+//       borderColor: 'rgb(255, 99, 132)',
+//       data: chartPrices,
+//     }]
+//   };
 
-  const configBar = {
-    type: 'bar',
-    data: data,
-    options: {}
-  };
+//   const configBar = {
+//     type: 'bar',
+//     data: data,
+//     options: {}
+//   };
 
-  const myBarChart = new Chart(
-    document.getElementById('myChart'),
-    configBar
-  );
+//   const myBarChart = new Chart(
+//     document.getElementById('myChart'),
+//     configBar
+//   );
